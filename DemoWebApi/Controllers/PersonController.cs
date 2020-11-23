9a8bc086d07b0381a5ab0b70a28320835fa200c9
Yalda Kheirkhah <71555131+yaldakheirkhah@users.noreply.github.com>
2020-11-23 22:16:06 +0330

    PersonController updated
    some api written

---
+++ DemoWebApi/Controllers/PersonController.cs
@@ -0,0 +1,48 @@
+﻿using DemoWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebApi.Controllers
+{
+    public class PersonController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PersonController()
+        {
+            people.Add(new Person { name = "yalda", lastname = "kheirkhah", id = 1 });
+            people.Add(new Person { name = "yal", lastname = "kheirkhah", id = 2 });
+            people.Add(new Person { name = "yalda", lastname = "kheir", id = 3 });
+        }
+        // GET: api/Person
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/Person/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.id == id).FirstOrDefault();
+        }
+
+        // POST: api/Person
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoWebApi/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebApi.Models
+{
+    public class Person
+    {
+
+        public int id { get; set; } = 0 ;
+        public string name { get; set; } = "" ;
+        public string lastname { get; set; } = "" ;
+    }
+}
